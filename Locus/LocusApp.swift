1b1d8e2069214a8803b7005d72738f2353662ca4
Nikolai Supereka <nsupereka92@gmail.com>
2024-10-29 17:40:06 +0300

    Initial Commit

---
+++ Locus/LocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocusApp.swift
+//  Locus
+//
+//  Created by Николай Суперека on 29.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct LocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LocusTests/LocusTests.swift
@@ -0,0 +1,17 @@
+//
+//  LocusTests.swift
+//  LocusTests
+//
+//  Created by Николай Суперека on 29.10.2024.
+//
+
+import Testing
+@testable import Locus
+
+struct LocusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
